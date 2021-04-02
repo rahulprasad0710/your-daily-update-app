0603e99bfa80202a5dc8e45f9e1f5459778eb0d8
rahulprasad0710 <raulshah2021@hotmail.com>
2021-04-03 01:21:11 +0530

    version 1

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Layout = ({ children }) => {
-    return <div className="layout col-md-8 box m-md-4 box ">{children}</div>;
+    return <div className="layout col-md-8  m-md-4 box ">{children}</div>;
 };
 
 export default Layout;
--- src/component/NewsBox.jsx
+++ src/component/NewsBox.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import Layout from "./Layout/Layout";
+import useFetchData from "./useFetchData";
+
 const NewsBox = () => {
+    const { fetchData: newsData, isLoading } = useFetchData(
+        "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3625826e70a34c18ad1c4a89b4a9f25b"
+    );
+    if (newsData) {
+        console.log(newsData);
+        var eachCard = newsData.data.articles.map((oneCard) => (
+            <div className="card d-flex ">
+                <img src={oneCard.urlToImage} alt="news " />
+                <div className="card-body">
+                    <h5 className="card-tittle">{oneCard.title}</h5>
+                    <p className="card-text">{oneCard.description}</p>
+                    <a href={oneCard.url} rel="noreferrer" target="_blank">
+                        Read More...
+                    </a>
+                </div>
+            </div>
+        ));
+    }
+
     return (
         <Layout>
             <h3>News Box</h3>
+            <div className="card-container">
+                {isLoading && (
+                    <span className="text-danger"> Loading.... </span>
+                )}
+                {newsData && eachCard}
+            </div>
         </Layout>
     );
 };
 
 export default NewsBox;
+
+//3625826e70a34c18ad1c4a89b4a9f25b
+//https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3625826e70a34c18ad1c4a89b4a9f25b
